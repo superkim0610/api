dffa8025298b7daa38f37fb7cf7ac7d86be4a343
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2021-10-23 20:09:01 +0900

    Add files via upload

---
+++ precache-manifest.cb1b27a7c66f9786e452ed9fbaa7ea9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdfd7fe9a927231af27",
+    "url": "/api/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/api/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fa19158bcc003b5659ab35249a7c2277",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "bf503ba0ce2462dca747",
+    "url": "/api/js/about.884513ea.js"
+  },
+  {
+    "revision": "bbdfd7fe9a927231af27",
+    "url": "/api/js/app.d185a465.js"
+  },
+  {
+    "revision": "8604d076bf72ce9cb82d",
+    "url": "/api/js/chunk-vendors.f5e17690.js"
+  },
+  {
+    "revision": "a3b87e5dd23d3e6c992f4798d31088a8",
+    "url": "/api/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/api/robots.txt"
+  }
+]);
